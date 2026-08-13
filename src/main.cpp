bdda087a3bf1bb0d0347c491bad13fadf86483d6
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-08-13 17:31:29 +0545

    Made the full code + debug with the help of my friend

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <Wifi.h>
+#include <WiFi.h>
 #include <WebServer.h>
 
 //defining the motor pins that i connected
@@ -43,7 +43,7 @@ pinMode(IN4,OUTPUT);
 ledcSetup(PWM_CH_A,PWM_FREQ,PWM_RES);
 ledcAttachPin(ENA,PWM_CH_A); 
 ledcSetup(PWM_CH_B,PWM_FREQ,PWM_RES);
-ledcAttachPin(ENB,PWM_CH_A); 
+ledcAttachPin(ENB,PWM_CH_B); 
 
 
 //creating own access point 
@@ -57,13 +57,13 @@ server.on("/F",HTTP_GET,[](){forward(); server.send(200,"text/plain","Forward");
 server.on("/B",HTTP_GET,[](){backward(); server.send(200,"text/plain","Backward");});
 server.on("/L",HTTP_GET,[](){left(); server.send(200,"text/plain","Left");});
 server.on("/R",HTTP_GET,[](){right(); server.send(200,"text/plain","Right");});
-server.on("/R",HTTP_GET,[](){stopMotor(); server.send(200,"text/plain","Stop");});
+server.on("/S",HTTP_GET,[](){stopMotor(); server.send(200,"text/plain","Stop");});
 
 
 server.on("/speed",HTTP_GET,[](){
   if(server.hasArg("v")){
     speedValue=server.arg("v").toInt(); //string to int
-    speedValue=constrain(speedValue,0,225); //clamping value form 0 tp 225
+    speedValue=constrain(speedValue,0,255); //clamping value form 0 tp 225
     ledcWrite(PWM_CH_A,speedValue);
     ledcWrite(PWM_CH_B,speedValue);
   }
@@ -77,7 +77,89 @@ void loop() {
   server.handleClient();
 }
 
-
-
-
+void handleRoot(){
+  String html =R"rawliteral(
+  <!DOCTYPE html><html>
+    <head>
+      <title>NRXN CAR</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+      body{ text-align: center; font-family:sans-serif;}
+      button{
+      width: 100px; height:40px;
+      font-size:16px; margin:10px;
+      }
+      input[type=range]{widht:80%}
+      </style>
+      <script>
+        function sendCommand(cmd){
+        fetch("/"+cmd);
+        }
+        //for the javascript fxn i.e button functionality 
+        function setupButton(id,command){
+        const btn =document.getElementById(id);
+        btn.addEventListener('mousedown',()=> sendCommand(command));
+        btn.addEventListener('mouseup', () => sendCommand('S'));
+          btn.addEventListener('touchstart', (e) => { e.preventDefault(); sendCommand(command); });
+          btn.addEventListener('touchend', (e) => { e.preventDefault(); sendCommand('S'); });
+        }
+        function setSpeed(val) {
+          fetch("/speed?v=" + val);
+          document.getElementById("speedLabel").innerText = val;
+        }
+        window.onload = () => {
+          setupButton("forward", "F");
+          setupButton("backward", "B");
+          setupButton("left", "L");
+          setupButton("right", "R");
+        };
+      </script>
+    </head>
+    <body>
+      <h2 style="text-align: center; font-weight:500; text-transform:uppercase">NRXN WEB CONTROLLED CAR</h2>
+      <div>
+        <button id="forward">Forward (F)</button><br>
+        <button id="left">Left(L)</button>
+        <button id="right">Right(R)</button><br>
+        <button id="backward">Backward(B)</button>
+      </div>
+      <p>Speed: <span id="speedLabel">200</span></p>
+        <input type="range" min="0" max="255" value="200" oninput="setSpeed(this.value)">
+      </div>
+    </body>
+    </html>
+        })rawliteral";
+        server.send(200,"text/html", html);
+}
+void forward(){
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+void backward(){
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+//motor A moves forward and B backward leading to left 
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);  
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);    
+}
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
 
